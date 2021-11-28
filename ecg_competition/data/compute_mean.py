b129530aa2a5cd06c8aaf7c78b14d524f8a7b52b
huxuepeng <763066931@qq.com>
2021-11-28 13:30:39 +0800

    数据标准化

--- ecg_competition/data/compute_mean.py
+++ ecg_competition/data/compute_mean.py
@@ -4,6 +4,8 @@ import numpy as np
 import cv2
 import random
 import os
+from scipy.io import loadmat
+from tqdm import tqdm
 
 """
     随机挑选CNum张图片，进行按通道计算均值mean和标准差std
@@ -11,40 +13,38 @@ import os
 """
 
 
-train_txt_path = os.path.join("..", "data/trainreference.csv")
+train_txt_path = os.path.join("ecg_competition", "data/train")
 
-CNum = 2000     # 挑选多少图片进行计算
+CNum = 500     # 挑选多少图片进行计算
 
-img_h, img_w = 32, 32
-imgs = np.zeros([img_w, img_h, 3, 1])
+imgs = np.zeros([12, 5000])
 means, stdevs = [], []
 
-with open(train_txt_path, 'r') as f:
-    lines = f.readlines()
-    random.shuffle(lines)   # shuffle , 随机挑选图片
+file_list = os.listdir(train_txt_path)
 
-    for i in range(CNum):
-        img_path = lines[i].rstrip().split()[0]
+for file in tqdm(file_list):
 
-        img = cv2.imread(img_path)
-        img = cv2.resize(img, (img_h, img_w))
+    data = loadmat(os.path.join(train_txt_path, file))
+    ecg_data = data["ecgdata"]
 
-        img = img[:, :, :, np.newaxis]
-        imgs = np.concatenate((imgs, img), axis=3)
-        print(i)
-
-imgs = imgs.astype(np.float32)/255.
+    imgs = np.concatenate((imgs, ecg_data), axis=1)
 
 
-for i in range(3):
-    pixels = imgs[:,:,i,:].ravel()  # 拉成一行
+print(imgs.shape)
+
+
+for i in range(12):
+    pixels = imgs[i,:].ravel()  # 拉成一行
     means.append(np.mean(pixels))
     stdevs.append(np.std(pixels))
 
-means.reverse() # BGR --> RGB
-stdevs.reverse()
+# means.reverse() # BGR --> RGB
+# stdevs.reverse()
 
 print("normMean = {}".format(means))
 print("normStd = {}".format(stdevs))
 print('transforms.Normalize(normMean = {}, normStd = {})'.format(means, stdevs))
 
+
+ormMean = [0.02102842710458631, 0.01870473789104995, -0.002323689225004715, -0.016818765162802455, 0.012285585395475473, 0.008799268047423623, -0.010402479687361263, -0.021703821823799017, -0.005407059704317367, 0.023126367419523692, 0.03341531410865756, 0.016268959850275762]
+normStd = [0.3053013037015497, 0.24986147063669015, 0.29824570044148807, 0.23575938566213706, 0.27472264903231897, 0.2288858965974916, 0.3108398675616677, 0.38992954676508795, 0.46233869773182745, 0.40961406719874305, 0.3935515632033764, 0.4898131524605047]
--- ecg_competition/data/dataset.py
+++ ecg_competition/data/dataset.py
@@ -3,6 +3,7 @@ import torch
 from scipy.io import loadmat
 import os
 import random
+from torchvision import transforms 
 
 class ECGDataset(Dataset):
     def __init__(self, data_path, train=True) -> None:
@@ -12,6 +13,8 @@ class ECGDataset(Dataset):
         print(data)
         self.data = data["train"] if train else data["val"]
         self.train = train
+        self.transformer = transforms.Normalize(normMean = [0.02102842710458631, 0.01870473789104995, -0.002323689225004715, -0.016818765162802455, 0.012285585395475473, 0.008799268047423623, -0.010402479687361263, -0.021703821823799017, -0.005407059704317367, 0.023126367419523692, 0.03341531410865756, 0.016268959850275762], 
+        normStd = [0.3053013037015497, 0.24986147063669015, 0.29824570044148807, 0.23575938566213706, 0.27472264903231897, 0.2288858965974916, 0.3108398675616677, 0.38992954676508795, 0.46233869773182745, 0.40961406719874305, 0.3935515632033764, 0.4898131524605047])
         
     def __len__(self):
         return self.data.shape[0]
